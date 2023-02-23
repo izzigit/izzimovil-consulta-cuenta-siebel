340549622e296ec70f42ac7e7a787281d25495d6
Carlos García Martínez <carlos.cg242@gmail.com>
2023-02-23 10:45:43 -0600

    aceptar JSON

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import siebelRouter from "./Routes/cvConsulta.route.js";
 const app = express();
 const PORT = process.env.PORT || 8898
 
+app.use(express.json());
 app.use('/cvSiebel/v1', siebelRouter)
 
 app.listen(PORT, ()=>{
